14bd2a9235f3da912ca15217b5dc13ead7662fd7
saad khan <iam.saadk@gmail.com>
2022-04-28 19:50:54 +0530

    added missing files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,58 @@
+import { useState, useRef } from 'react';
+//Import Styling components
+import './styles/app.scss';
+
+//Import Components
+import Songs from './components/songs';
+import Player from './components/player';
+import Library from './components/library';
+//Import Ulit
+import SongsData from './util';
+
 function App() {
-	return <div className='App'>
-    <h1>Music Player</h1>
-  </div>;
+	//Refs
+	const audioRef = useRef(null);
+
+	//States
+	const [songs, setSongs] = useState(SongsData());
+	const [currentSong, setCurrentSong] = useState(songs[0]);
+	const [isPlaying, setIsPlaying] = useState(false);
+	const [songInfo, setSongInfo] = useState({
+		currentTime: 0,
+		duration: 0,
+	});
+
+	//Event Handler
+	const timeUpdateHandler = (e) => {
+		const current = e.target.currentTime;
+		const duration = e.target.duration;
+		setSongInfo({ ...songInfo, currentTime: current, duration });
+	};
+
+	return (
+		<div className='App'>
+			<Songs currentSong={currentSong} />
+			<Player
+				isPlaying={isPlaying}
+				setIsPlaying={setIsPlaying}
+				songInfo={songInfo}
+				setSongInfo={setSongInfo}
+				audioRef={audioRef}
+			/>
+			<Library
+				songs={songs}
+				setSongs={setSongs}
+				setCurrentSong={setCurrentSong}
+				isPlaying={isPlaying}
+				audioRef={audioRef}
+			/>
+			<audio
+				ref={audioRef}
+				onTimeUpdate={timeUpdateHandler}
+				onLoadedMetadata={timeUpdateHandler}
+				src={currentSong.audio}></audio>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/util.js
@@ -0,0 +1,108 @@
+import { v4 as uudiv4 } from 'uuid';
+
+const SongsData = () => {
+	return [
+		{
+			name: 'Blue Moment',
+			cover:
+				'https://chillhop.com/wp-content/uploads/2022/04/7e2c880b50c6647e59330b1c371101ce2950625f-1024x1024.jpg',
+			artist: ' Sugi.wa, Tom Ford ',
+			audio: 'https://mp3.chillhop.com/serve.php/?mp3=32978',
+			color: ['#DACEB4', '#938384'],
+			id: uudiv4(),
+			active: false,
+		},
+		{
+			name: 'Parasol',
+			cover:
+				'https://chillhop.com/wp-content/uploads/2022/04/c46e7efae453e56f839e3683926e809639f167b0-1024x1024.jpg',
+			artist: ' Plusma, Guillaume Muschalle, Thomas Renwick ',
+			audio: 'https://mp3.chillhop.com/serve.php/?mp3=28901',
+			color: ['#0E314C', '#6D7866'],
+			id: uudiv4(),
+			active: false,
+		},
+		{
+			name: 'Moody',
+			cover:
+				'https://chillhop.com/wp-content/uploads/2022/04/8cc8bdc59b30dd1f9d401cb485b8537d909e1f50-1024x1024.jpg',
+			artist: 'C Y G N',
+			audio: 'https://mp3.chillhop.com/serve.php/?mp3=31598',
+			color: ['#7E887D', '#349053'],
+			id: uudiv4(),
+			active: false,
+		},
+		{
+			name: 'Santiago',
+			cover:
+				'https://chillhop.com/wp-content/uploads/2022/01/1abf44fbb1364ca8435a8bbf4c5750a80947128f-1024x1024.jpg',
+			artist: ' Psalm Trees, Moods ',
+			audio: 'https://mp3.chillhop.com/serve.php/?mp3=30202',
+			color: ['#8092A6', '#080808'],
+			id: uudiv4(),
+			active: false,
+		},
+		{
+			name: 'Glow',
+			cover:
+				'https://chillhop.com/wp-content/uploads/2022/03/efb56e836d82d26e857b341a951f0337ace66b8b-1024x1024.jpg',
+			artist: 'Tesk',
+			audio: 'https://mp3.chillhop.com/serve.php/?mp3=32888',
+			color: ['#21221C', '#862F59'],
+			id: uudiv4(),
+			active: false,
+		},
+		{
+			name: 'Letters',
+			cover:
+				'https://chillhop.com/wp-content/uploads/2021/12/754c5a4edf9d3bb338dfe89062760f22b7aaec70-1024x1024.jpg',
+			artist: ' Oh Caroline, Lhasa Petik, Towerz ',
+			audio: 'https://mp3.chillhop.com/serve.php/?mp3=30131',
+			color: ['#1B3848', '#B24C49'],
+			id: uudiv4(),
+			active: false,
+		},
+		{
+			name: 'Ending',
+			cover:
+				'https://chillhop.com/wp-content/uploads/2022/01/0ac63dcdc4e16e6bb979f6ea2c9d0a394ee9e386-1024x1024.jpg',
+			artist: ' sadtoi, Relyae ',
+			audio: 'https://mp3.chillhop.com/serve.php/?mp3=28969',
+			color: ['#373361', '#073644'],
+			id: uudiv4(),
+			active: false,
+		},
+		{
+			name: 'la zona',
+			cover:
+				'https://chillhop.com/wp-content/uploads/2021/11/3ff29a35be582c8dc0222273cb9c401ea6b209dc-1024x1024.jpg',
+			artist: 'Maydee',
+			audio: 'https://mp3.chillhop.com/serve.php/?mp3=27455',
+			color: ['#AFC0D7', '#6A5834'],
+			id: uudiv4(),
+			active: false,
+		},
+		{
+			name: 'Soulsounds',
+			cover:
+				'https://chillhop.com/wp-content/uploads/2021/11/4c9682ee612a3b8ef51de286c49b5489408e9257-1024x1024.jpg',
+			artist: 'Parkbench Epiphany ',
+			audio: 'https://mp3.chillhop.com/serve.php/?mp3=27500',
+			color: ['#F19797', '#1B2635'],
+			id: uudiv4(),
+			active: false,
+		},
+		{
+			name: 'Looking In',
+			cover:
+				'https://chillhop.com/wp-content/uploads/2021/08/af3ce13ad39d38057f00144f8a7c295877ccfec1-1024x1024.jpg',
+			artist: ' Smile High, Teddy Roxpin ',
+			audio: 'https://mp3.chillhop.com/serve.php/?mp3=23335',
+			color: ['#43878F', '#5EB0B4'],
+			id: uudiv4(),
+			active: false,
+		},
+	];
+};
+
+export default SongsData;
